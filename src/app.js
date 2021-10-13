7df5c64cc4be1add41b9f9ef5d6eb97c2a71d2a8
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-10-13 18:09:02 +0300

    3

--- src/app.js
+++ src/app.js
@@ -4,7 +4,5 @@ const characters = [
   { name: 'маг', health: 0 },
   { name: 'лучник', health: 0 },
 ];
-
+// eslint-disable-next-line no-unused-vars
 const alive = characters.filter((item) => item.health > 0);
-
-alive[0].health = 0;//изменили для корректности работы eslint
